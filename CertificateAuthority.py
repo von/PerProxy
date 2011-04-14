75fa91c4907d2d9c639bd47a170c3eef13214701
Von Welch <von@vwelch.com>
2011-04-13 22:04:22 -0400

    Added a good start of a generate_ssl_credential() method.

--- CertificateAuthority.py
+++ CertificateAuthority.py
@@ -1,11 +1,13 @@
 """Certificate Authority"""
 
-from M2Crypto import EVP, X509
+from M2Crypto import EVP, m2, RSA, X509
 
 class CertificateAuthority:
-    def __init__(self, cert, key):
+    def __init__(self, cert, key, serial_number = None):
         self.cert = cert
         self.key = key
+        self.serial_number = serial_number \
+            if serial_number is not None else cert.get_serial_number() + 1
 
     @classmethod
     def from_file(cls, cert_file, key_file):
@@ -13,3 +15,43 @@ class CertificateAuthority:
         key = EVP.load_key(key_file)
         return cls(cert, key)
 
+    def generate_ssl_credential(self,
+                                hostname,
+                                key_length=2048,
+                                lifetime=24*60*60,
+                                sign_hash="sha1"):
+        """Generate credentials for a given target.
+
+        Returns a tuple of X509 certificate and EVP key."""
+        rsa_key = RSA.gen_key(key_length, m2.RSA_F4)
+        key = EVP.PKey()
+        key.assign_rsa(rsa_key)
+        cert = X509.X509()
+        cert.set_serial_number(self.serial_number)
+        self.serial_number += 1
+        cert.set_version(2)
+        name = self.get_relative_subject()
+        name.CN = hostname
+        cert.set_subject(name)
+        cert.set_issuer(self.cert.get_subject())
+        cert.set_pubkey(key)
+        notBefore = m2.x509_get_not_before(cert.x509)
+        notAfter  = m2.x509_get_not_after(cert.x509)
+        m2.x509_gmtime_adj(notBefore, 0)
+        m2.x509_gmtime_adj(notAfter, lifetime)
+        ext = X509.new_extension('basicConstraints', 'CA:FALSE')
+        ext.set_critical()
+        cert.add_ext(ext)
+        ext = X509.new_extension('keyUsage',
+                                 'digitalSignature, keyEncipherment')
+        ext.set_critical()
+        cert.add_ext(ext)
+        cert.sign(self.key, sign_hash)
+        return cert, key
+
+    def get_relative_subject(self):
+        """Return a X509_NAME wthout the CN field set suitable for a EEC signed by the CA"""
+        name = X509.X509_Name()
+        ca_name = self.cert.get_subject()
+        name.O = "My Org"  # TODO: Make this configurable
+        return name
--- unittests/CertificateAuthority_test.py
+++ unittests/CertificateAuthority_test.py
@@ -6,11 +6,25 @@ import unittest
 class TestCertificateAuthority(unittest.TestCase):
     """Tests for CertificateAuthority class"""
 
+    def _load_ca(self):
+        """Load and return a test CA"""
+        from CertificateAuthority import CertificateAuthority
+        return CertificateAuthority.from_file("ca-cert.pem", "ca-key.pem")
+
     def test_from_file(self):
         """Test load_file()"""
         from CertificateAuthority import CertificateAuthority
         ca = CertificateAuthority.from_file("ca-cert.pem", "ca-key.pem")
 
+    def test_generate_ssl_credential(self):
+        """Test generate_ssl_credential()"""
+        ca = self._load_ca()
+        cert, key = ca.generate_ssl_credential("www.example.com")
+        self.assertIsNotNone(cert)
+        self.assertIsNotNone(key)
+        subject = cert.get_subject().as_text()
+        # XXX: This assumed hard-coded O component
+        self.assertEqual(subject, "O=My Org, CN=www.example.com")
 
 if __name__ == "__main__":
     unittest.main()
